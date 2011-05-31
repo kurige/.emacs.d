6db5a7abcc13d46ed1bd2bed3f8b9b9aad529906
chrisg <christopher.gateley@gmail.com>
2011-05-31 00:55:11 -0500

    First stab at getting flyspell working with hunspell

--- init.el
+++ init.el
@@ -41,3 +41,41 @@
 
 (set-frame-font "Menlo-11")
 (require 'zenburn-theme)
+
+;; Attempt #1
+
+; Use hunspell instead of ispell
+; http://www.emacswiki.org/emacs/InteractiveSpell#toc6
+;(setq ispell-program-name "/usr/local/bin/hunspell")
+;(require 'rw-language-and-country-codes)
+;(require 'rw-ispell)
+;(require 'rw-hunspell)
+;(setq ispell-dictionary "en_US")
+;(custom-set-variables
+; '(rw-hunspell-default-dictionary "en_US")
+; '(rw-hunspell-dicpath-list (quote ("/Users/chris/.emacs.d/dict/")))
+; '(rw-hunspell-make-dictionary-menu t)
+; '(rw-hunspell-use-rw-ispell t)
+;)
+
+;; Attempt #2
+
+;(eval-after-load "ispell"
+;   (progn
+;     (setq ispell-dictionary "american"
+;           ispell-extra-args '("-a" "-i" "utf-8")
+;           ispell-silently-savep t
+;           ispell-dictionary-alist
+;           '(("nil" ; "nil" default
+;              "[A-Za-z]" "[^A-Za-z]" "[']" t ("-d" "en_US" "-i" "utf-8") nil utf-8)
+;             ("american" ; Yankee English
+;              "[A-Za-z]" "[^A-Za-z]" "[']" t ("-d" "en_US" "-i" "utf-8") nil utf-8)
+;             ("british" ; British English
+;              "[A-Za-z]" "[^A-Za-z]" "[']" t ("-d" "en_GB" "-i" "utf-8") nil utf-8)
+;             ("german"
+;              "[a-zäöüßA-ZÄÖÜ]" "[^a-zäöüßA-ZÄÖÜ]" "[']" t ("-d" "de_DE" "-i" "utf-8") nil utf-8)
+;             ("german8"
+;              "[a-zäöüßA-ZÄÖÜ]" "[^a-zäöüßA-ZÄÖÜ]" "[']" t ("-d" "de_DE" "-i" "utf-8") nil utf-8)
+;             ))))
+;
+;(setq-default ispell-program-name "/usr/local/bin/hunspell")
