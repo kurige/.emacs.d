e7cfcda2cbcb32532003b75ec76c52186a9d1343
Christopher Gateley <christopher.gateley@gmail.com>
2011-06-12 15:04:58 -0600

    Add install code to init.el for first run

--- init.el
+++ init.el
@@ -16,6 +16,16 @@
 ;; ---------------
 
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+;; Detect if el-get is installed, and install it if it's not.
+;; (Unfortunately this requires an internet connection at program start.)
+(unless (require 'el-get nil t)
+  (url-retrieve
+   "https://github.com/dimitri/el-get/raw/master/el-get-install.el"
+   (lambda (s)
+     (end-of-buffer)
+     (eval-print-last-sexp))))
+
 (require 'el-get)
 
 (setq el-get-sources
