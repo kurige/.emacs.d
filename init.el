cb576ff0fc0b27174efeaf888b8692d738ef2a95
chrisg <christopher.gateley@gmail.com>
2011-06-12 15:50:42 -0500

    Remove unnecessary packages from el-get load path

--- init.el
+++ init.el
@@ -19,20 +19,20 @@
 (require 'el-get)
 
 (setq el-get-sources
-      '(el-get switch-window yasnippet
-       (:name magit
-              :after (lambda () (global-set-key (kbd "C-x C-z") 'magit-status)))
-       (:name zenburn-theme :type http
-              :url "https://github.com/djcb/elisp/raw/master/themes/zenburn-theme.el")))
+      '(switch-window
+        (:name magit
+               :after (lambda () (global-set-key (kbd "C-x C-z") 'magit-status)))
+        (:name zenburn-theme :type http
+               :url "https://github.com/djcb/elisp/raw/master/themes/zenburn-theme.el")))
 
-(el-get 'wait)
+(el-get)
 
 ;; Local packages
 ;; --------------
 
 (let ((default-directory
         (concat user-emacs-directory
-                (convert-standard-filename "site-lisp/"))))
+                (convert-standard-filename "site-packages/"))))
   (normal-top-level-add-to-load-path '("."))
   (normal-top-level-add-subdirs-to-load-path))
 
@@ -41,3 +41,32 @@
 
 (set-frame-font "Menlo-11")
 (require 'zenburn-theme)
+
+;; Show column number
+(column-number-mode)
+
+;; Set up org-mode latex export
+(require 'org-latex)
+(unless (boundp 'org-export-latex-classes)
+  (setq org-export-latex-classes nil))
+
+;; To specify a specific LaTeX class, add the following to the top of your org file:
+;;   #LaTeX_CLASS: <class>
+;; Where <class> can be one of: article, koma-article.
+;; Some additional options that you may want to include:
+;;   #+LaTeX_CLASS_OPTIONS: [a4paper, twoside, twocolumn]
+;; And for koma-script, to type-set table captions properly:
+;;   #+LaTeX_CLASS_OPTIONS: [captions=tableheading]
+
+(add-to-list 'org-export-latex-classes
+             '("article"
+               "\\documentclass{article}"
+               ("\\section{%s}" . "\\section*{%s}")))
+(add-to-list 'org-export-latex-classes
+             '("koma-article"
+               "\\documentclass{scrartcl}"
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+               ("\\paragraph{%s}" . "\\paragraph*{%s}")
+               ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
